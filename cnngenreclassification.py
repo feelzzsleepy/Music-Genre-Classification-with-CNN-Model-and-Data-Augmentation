036cf21f5b1e54237d05d9c04de79ca5d99188e3
feelzzsleepy <153752772+feelzzsleepy@users.noreply.github.com>
2023-12-14 04:07:11 +0800

    Update cnngenreclassification.py

--- cnngenreclassification.py
+++ cnngenreclassification.py
@@ -8,7 +8,7 @@ Original file is located at
 
 # Genre Classification CNN Model with Data Augmentation
 """
-!pip install streamlit matplotlib
+
 import streamlit as st
 import pandas as pd
 import numpy as np
